b360c4c2d321c0b318426f69c00b25f848a13486
Sunny Bhardwaj <sunny13nitk@gmail.com>
2024-08-01 17:02:19 +0530

    commit

--- src/components/ToDo/api/ApiClient.js
+++ src/components/ToDo/api/ApiClient.js
@@ -4,6 +4,6 @@ import axios from "axios";
 export const apiClient = axios.create(
     {
         //baseURL: 'http://localhost:8080'
-         baseURL: 'http://ws-todos-env.eba-cve7nqvf.ap-south-1.elasticbeanstalk.com'
+         baseURL: 'http://ws-todos-env.eba-cve7nqvf.ap-south-1.elasticbeanstalk.com' 
     }
 )
