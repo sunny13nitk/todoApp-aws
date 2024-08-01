e7e247f27d959efbad4c65687d26af65923131f8
Sunny Bhardwaj <sunny13nitk@gmail.com>
2024-08-01 16:17:51 +0530

    base url to aws BS deployed- url

--- src/components/ToDo/api/ApiClient.js
+++ src/components/ToDo/api/ApiClient.js
@@ -3,6 +3,7 @@ import axios from "axios";
 
 export const apiClient = axios.create(
     {
-        baseURL: 'http://localhost:8080'
+        //baseURL: 'http://localhost:8080'
+         baseURL: 'http://ws-todos-env.eba-cve7nqvf.ap-south-1.elasticbeanstalk.com'
     }
 )
